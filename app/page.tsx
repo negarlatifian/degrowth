fc7092e88b4a932c0f496738b83d0d97698cb5be
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2024-04-01 12:24:33 +0200

    added style to left

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className='flex flex-row'>
+    <main className='flex flex-row bg-white'>
       <Left />
       <Right />
     </main>
--- components/Left.tsx
+++ components/Left.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Left = () => {
-  return <div>Left</div>;
+  return (
+    <div className='flex flex-col h-lvh w-1/3 mt-[3rem] ml-[3.5rem]'>
+      <h2 className='font-display'>About</h2>
+      <ul className='flex flex-col mt-auto mb-[5rem]'>
+        <li className='font-display'>Intro</li>
+        <li className='font-display'>Degrowth</li>
+        <li className='font-display'>Values</li>
+        <li className='font-display'>Disavowals</li>
+        <li className='font-display'>Strategies</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Left;
