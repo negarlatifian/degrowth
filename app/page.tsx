5c327f5b00d319156d1fd6731b22002e8b9f9acd
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2024-04-01 12:04:22 +0200

    added right and left components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import Left from '@/components/Left';
+import Hero from '@/components/Left';
+import Right from '@/components/Right';
 import Image from 'next/image';
 
 export default function Home() {
-  return <main className=''></main>;
+  return (
+    <main className='flex flex-row'>
+      <Left />
+      <Right />
+    </main>
+  );
 }
---
+++ components/Left.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Left = () => {
+  return <div>Left</div>;
+};
+
+export default Left;
