aa0d64c771f8557ea354ac27d642be04d0708d2e
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2024-04-02 09:33:56 +0200

    hero mobile done

--- components/right/Hero.tsx
+++ components/right/Hero.tsx
@@ -2,13 +2,19 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div className='flex flex-col mt-[3rem] '>
-      <h1 className='font-text text-[2.4rem] leading-10 mb-[1rem]'>
+    <div className='flex flex-col xs:mt-[3rem] mt-[5rem] ml-[2rem] '>
+      <h1 className='font-text xs:text-[2.4rem] text-[1.6rem] leading-7 xs:leading-10 mb-[1rem]'>
         The Degrowth Toolbox
         <br />
         For Artistic Practices
       </h1>
-      <Image src='/Images/1.jpg' alt='hero' height={642} width={938} />
+      <Image
+        src='/Images/1.jpg'
+        alt='hero'
+        height={642}
+        width={938}
+        className=' h-[30rem] max-w-[180%] relative'
+      />
     </div>
   );
 };
